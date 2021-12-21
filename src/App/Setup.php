4b6a8ed4b9128599321260f1b8bbaaffeb953b36
kekefreedog <kevin.zarshenas@gmail.com>
2021-12-21 10:22:49 +0100

    Add comment config

--- src/App/Setup.php
+++ src/App/Setup.php
@@ -105,7 +105,7 @@ class Setup{
         $this->input = Arrays::stretch($this->input, "_");
 
         # Wrtie input in config > app.yml
-        file_put_contents(__ROOT_APP__.'config/app.yml', Yaml::dump($this->input, 10));
+        file_put_contents(__ROOT_APP__.'config/app.yml', "# Configuration of the app".PHP_EOL.Yaml::dump($this->input, 10));
 
     }
 
