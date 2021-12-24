c060a6b9a09f13a20989fdd68170c180d5aed87b
Kévin Zarshenas <kevin.zarshenas@gmail.com>
2021-12-24 01:57:30 +0100

    Fix setup

--- src/File/Structure.php
+++ src/File/Structure.php
@@ -105,7 +105,7 @@ class Structure extends Files{
                         ){
 
                             # Get new content
-                            $newContent = $this->{$fileContent['function']['name']}(...([$filepath] + ($fileContent['function']['parameters'] ?? [])));
+                            $newContent = $this->{$fileContent['function']['name']}(...([$filepath] + ($fileContent['function']['arguments'] ?? [])));
 
                             # Put new content in file
                             file_put_contents($filepath, $newContent);
